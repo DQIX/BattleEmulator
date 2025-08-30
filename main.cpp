5dcacade287eb14a06f192571be330ba5e571a08
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-08-30 13:24:09 +0900

    思考難易度を250000に変更

--- main.cpp
+++ main.cpp
@@ -493,7 +493,7 @@ namespace {
         }
 
         auto genome =
-                ActionOptimizer::RunAlgorithm(copiedPlayers, seed, turns, 100000, gene, numThreads);
+                ActionOptimizer::RunAlgorithm(copiedPlayers, seed, turns, 250000, gene, numThreads);
 
         auto turnProcessed = BattleEmulator::getTurnProcessed();
         std::optional<BattleResult> result1;
