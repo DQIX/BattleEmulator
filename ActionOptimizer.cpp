8d640b4d20fcb695b300319b86f192d823d786e5
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-01-15 17:11:14 +0900

    rework

--- ActionOptimizer.cpp
+++ ActionOptimizer.cpp
@@ -193,7 +193,7 @@ Genome ActionOptimizer::RunAlgorithm(const Player players[2], uint64_t seed, int
             if (currentGenome.turn > startT) {
                 continue;
             }
-            if (solutionFound && currentGenome.turn > bestSolution.turn) {
+            if (solutionFound && currentGenome.turn > bestSolution.turn - 1) {
                 continue;
             }
 
