1940137fed2b2e1224714fe2b571286435df5544
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-01-18 15:44:23 +0900

    まあええや

--- ActionOptimizer.cpp
+++ ActionOptimizer.cpp
@@ -118,7 +118,7 @@ Genome ActionOptimizer::RunAlgorithm(const Player players[2], uint64_t seed, int
     std::unique_ptr<int> position = std::make_unique<int>(1);
     std::unique_ptr<uint64_t> nowState = std::make_unique<uint64_t>(0);
 
-    LinearIdPool<Genome, 180000> Pool{};
+    LinearIdPool<Genome, 100000> Pool{};
 
     // Enhanced A* priority queue and visited set
     EnhancedHeapQueue openSet{};
@@ -184,7 +184,7 @@ Genome ActionOptimizer::RunAlgorithm(const Player players[2], uint64_t seed, int
 
     auto lastimp = 0;
 
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 2; ++i) {
         if (!solutionFound) {
             maxGenerations *= 2;
         }else {
