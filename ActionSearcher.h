9c490534b6e6efd43b9c9bf1968489bec73cf7c8
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-01-25 18:21:19 +0900

    工場出荷時の状態

--- ActionSearcher.h
+++ ActionSearcher.h
@@ -18,7 +18,7 @@ struct SearchPlan {
 class ActionSearcher {
 public:
     static constexpr int MAX_LAYER = 1024;
-    static constexpr int MAX_BEAM = 512;
+    static constexpr int MAX_BEAM = 150;
     static constexpr int MAX_DEPTH = 5;
     static constexpr int BEST_LIMIT = 20;
 
