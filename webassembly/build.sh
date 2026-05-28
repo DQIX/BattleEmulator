e1438becfc52579e184e708e18db362c37b6c896
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-05-28 17:49:04 +0900

    fix

--- webassembly/build.sh
+++ webassembly/build.sh
@@ -29,9 +29,9 @@ done
 # ------------------------------------------------------------
 declare -A BRANCH_VARIANTS
 
-BRANCH_VARIANTS[brurudoga_v9_new_arugo]="
-brurudoga_v9_tate: -Dtate=1
-brurudoga_v9_no_tate: -Dno_tate=1
+BRANCH_VARIANTS[burudoga_v9_new_arugo]="
+burudoga_v9_tate: -Dtate=1
+burudoga_v9_no_tate: -Dno_tate=1
 "
 
 
